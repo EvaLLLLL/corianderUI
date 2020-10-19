a2953d0c3b4a4aac3ec902b4ef01b58ca0768b36
lyhhh <17610603221@163.com>
2020-10-19 22:26:23 +0800

    创建 Menu 组件

---
+++ src/components/MenuDemo.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h2>Menus示例</h2>
+		<Menus v-model:openSubs="openSubs"
+		       v-model:selectedItems="selectedItems">
+			<SubMenu title="副菜单A" subkey="sub1">
+				<Menu itemkey="1">小菜单1</Menu>
+				<Menu itemkey="2">小菜单2</Menu>
+				<Menu itemkey="3">小菜单3</Menu>
+			</SubMenu>
+			<SubMenu title="副菜单B" subkey="sub2">
+				<Menu itemkey="4">小菜单4</Menu>
+				<Menu itemkey="5">小菜单5</Menu>
+				<Menu itemkey="6">小菜单6</Menu>
+			</SubMenu>
+			<SubMenu title="副菜单C" subkey="sub3">
+				<Menu itemkey="7">小菜单7</Menu>
+				<Menu itemkey="8">小菜单8</Menu>
+				<Menu itemkey="9">小菜单9</Menu>
+			</SubMenu>
+		</Menus>
+	</div>
+</template>
+
+<script lang="ts">
+	import {ref} from 'vue';
+	import Menus from '../lib/Menus.vue';
+	import Menu from '../lib/Menu.vue';
+	import SubMenu from '../lib/SubMenu.vue';
+	
+	export default {
+		components: {Menus, Menu, SubMenu},
+		setup() {
+			const openSubs = ref<string[]>(['sub1']);
+			const selectedItems = ref<string[]>(['1']);
+			return {selectedItems, openSubs};
+		}
+	};
+</script>
---
+++ src/lib/SubMenu.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="xc-menus">
+		<div class="menuGroups">
+			<div class="xc-menus-subtitle">{{props.title}}</div>
+			<div class="xc-menus-items"></div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	export default {
+		props: {
+			title: {
+				type: String
+			},
+			subkey: {
+				type: String
+			}
+		},
+		setup(props, context) {
+			console.log(props);
+		}
+	};
+</script>
