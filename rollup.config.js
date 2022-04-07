afb33d9f97f2a486f737d37589bab48f911ff97f
lyhhh <17610603221@163.com>
2022-04-08 03:29:38 +0800

    add external

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,38 @@
-import esbuild from "rollup-plugin-esbuild";
-import vue from "rollup-plugin-vue";
-import scss from "rollup-plugin-scss";
-import dartSass from "sass";
-import { terser } from "rollup-plugin-terser";
+import esbuild from 'rollup-plugin-esbuild'
+import vue from 'rollup-plugin-vue'
+import scss from 'rollup-plugin-scss'
+import dartSass from 'sass'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: "src/lib/index.ts",
+  input: 'src/lib/index.ts',
   output: [
     {
       globals: {
-        vue: "Vue",
+        vue: 'Vue',
       },
-      name: "coriander-ui",
-      file: "dist/lib/coriander.js",
-      format: "umd",
+      name: 'coriander-ui',
+      file: 'dist/lib/coriander.js',
+      format: 'umd',
       plugins: [terser()],
     },
     {
-      name: "coriander-ui",
-      file: "dist/lib/coriander.esm.js",
-      format: "es",
+      name: 'coriander-ui',
+      file: 'dist/lib/coriander.esm.js',
+      format: 'es',
       plugins: [terser()],
     },
   ],
+  external: ['github-markdown-css', 'marked', 'prismjs', 'vue', 'vue-router'],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
       include: /\.[jt]s$/,
-      minify: process.env.NODE_ENV === "production",
-      target: "es2015",
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2015',
     }),
     vue({
       include: /\.vue$/,
     }),
   ],
-};
+}
